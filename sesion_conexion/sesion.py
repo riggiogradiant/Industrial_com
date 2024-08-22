db0646416af17f42d2a20b842fc06a63f7d2bf9e
riggiogradiant <criggio@gradiant.org>
2024-08-22 10:39:20 +0200

    Funcionando para todo tipo de sesiones (1 o más conexiones)

--- sesion_conexion/sesion.py
+++ sesion_conexion/sesion.py
@@ -18,7 +18,6 @@ def handle_connection(conn, addr):
     print(f"Conexion {addr} cerrada")
     conn.close()
 
-
 def check_threads_alive(threads):
     """
     Esta función recibe una lista de hilos y devuelve True si algún hilo está activo,
@@ -26,50 +25,55 @@ def check_threads_alive(threads):
     """
     return any(thread.is_alive() for thread in threads)
 
-
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(("0.0.0.0", 5020))
     sock.listen(1)
     print("Servidor escuchando en el puerto 5020")
+
     threads = []
+    first_connection = False
+    timeout_duration = 20
 
     try:
         while True:
-            conn, addr = sock.accept()
+            if first_connection:
+                sock.settimeout(timeout_duration)  # Set t-out after 1 connection
+            try:
+                conn, addr = sock.accept()
+            except socket.timeout:  # If t-out -> no more connections on that session
+                print(f"[+] FIN DE SESIÓN DE SOLO 1 CONEXIÓN")
+                threads.clear()
+                sock.settimeout(None)
+                first_connection = False
+                continue
+
             print(f"Conexion establecida con {addr}")
+            first_connection = True  # First connection established
             thread = threading.Thread(target=handle_connection, args=(conn, addr))
             thread.start()
             threads.append(thread)
 
-            th_alive = True
+            sleep(0.5)
 
-            if th_alive:
-                # Imprimir el estado de todos los hilos
-                print("Hilos totales: ",
-                      [(thread.name, thread.ident, thread.is_alive(), thread.native_id) for thread in threads])
-
-                # Imprimir el estado de los hilos activos
-                print("Hilos activos: ",
-                      [(thread.name, thread.ident, thread.is_alive(), thread.native_id) for thread in threads if
-                       thread.is_alive()])
-
-                # Imprimir el resultado de check_threads_alive
-                print("¿Hay hilos activos?:", check_threads_alive(threads))
-
-                sleep(0.5)  # Espera antes de volver a comprobar el estado de los hilos
-
-            print(" [**] Hilos totales: ",
+            print("Hilos totales: ",
                   [(thread.name, thread.ident, thread.is_alive(), thread.native_id) for thread in threads])
+
+            print("Hilos activos: ",
+                  [(thread.name, thread.ident, thread.is_alive(), thread.native_id) for thread in threads if
+                   thread.is_alive()])
+
+            # Check if all threads are alive and more than 1
             if not check_threads_alive(threads) and len(threads) > 1:
                 print("[+] FIN DE LA SESIÓN")
+                sock.settimeout(None)
+                first_connection = False
                 threads.clear()
-
 
     except KeyboardInterrupt:
         print("\nServidor detenido.")
     finally:
-        print("FIN DE TODOS LOS THREADS")
+        print("FIN DEL PROGRAMA")
         sock.close()
 
 
